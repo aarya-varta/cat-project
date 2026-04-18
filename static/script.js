9462f901014da56816dac7ba1654499ca4579280
Aarya-Personal <aarya00095@gmail.com>
2026-04-18 22:04:58 +0530

    -

--- static/script.js
+++ static/script.js
@@ -1,10 +1,25 @@
-async function getCat() {
-    const response = await fetch("/random-cat");
-    const data = await response.json();
+const btn = document.getElementById("cat-btn");
+const card = document.getElementById("card");
 
-    document.getElementById("fact").innerText = data.fact;
+btn.addEventListener("click", async () => {
+    btn.innerText = "Loading... 😼";
+    btn.disabled = true;
 
-    const img = document.getElementById("cat-img");
-    img.src = data.image + "?t=" + new Date().getTime(); // avoid caching
-    
-}
+    try {
+        const res = await fetch("/random-cat");
+        const data = await res.json();
+
+        document.getElementById("fact").innerText = data.fact;
+
+        const img = document.getElementById("cat-img");
+        img.src = data.image + "?t=" + new Date().getTime();
+
+        card.classList.remove("hidden");
+
+    } catch (err) {
+        alert("Something broke 😿");
+    }
+
+    btn.innerText = "Another one 😺";
+    btn.disabled = false;
+});
